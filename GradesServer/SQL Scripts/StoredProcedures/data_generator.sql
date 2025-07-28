c7970034c259c513f12ace27211c4464faeade71
erezdan <erezdanmail@gmail.com>
2025-07-28 12:29:49 +0300

    fix the numTests

--- GradesServer/SQL Scripts/StoredProcedures/data_generator.sql
+++ GradesServer/SQL Scripts/StoredProcedures/data_generator.sql
@@ -1,11 +1,4 @@
-﻿USE [Grades]
-GO
-/****** Object:  StoredProcedure [dbo].[data_generator]    Script Date: 28/07/2025 12:12:23 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-ALTER PROCEDURE [dbo].[data_generator]
+﻿ALTER PROCEDURE [dbo].[data_generator]
     @NumSubjects INT,
     @NumZonesPerSubject INT,
     @NumQuestionsPerZone INT,
@@ -21,19 +14,17 @@ BEGIN
     -- Step 2: First create all subjects in snapshot 0 using simple_migration_script
     DECLARE @OriginalSubjectIds TABLE (SubjectId INT);
     DECLARE @i INT = 1;
-    
+
     WHILE @i <= @NumSubjects
     BEGIN
         DECLARE @OriginalSubjectId INT;
 
-        -- Create initial subject in snapshot 0
         EXEC simple_migration_script
             @SnapshotId = 0,
             @NumZones = @NumZonesPerSubject,
             @NumQuestionsPerZone = @NumQuestionsPerZone,
             @SubjectId = @OriginalSubjectId OUTPUT;
 
-        -- Add error handling for the procedure call
         IF @OriginalSubjectId IS NULL
         BEGIN
             RAISERROR('simple_migration_script failed to return a valid SubjectId', 16, 1);
@@ -44,16 +35,29 @@ BEGIN
         SET @i += 1;
     END
 
-    -- Step 3: Now clone ALL catalog data from snapshot 0 to new snapshot
+    -- Step 3: Create new tests and initialize stats table
+    DECLARE @Tests TABLE (RowNum INT IDENTITY(1,1), TestId INT);
+    DECLARE @TestStats TABLE (TestId INT PRIMARY KEY, QuestionCount INT DEFAULT 0);
 
-    -- Clone Tests (Tests table doesn't have SnapshotId, but we need to map for Questions)
-    DECLARE @TestMap TABLE (OldTestId INT, NewTestId INT);
-    INSERT INTO @TestMap (OldTestId, NewTestId)
-    SELECT TestId, TestId FROM Tests; -- Tests don't get duplicated, just mapped
+    DECLARE @t INT = 1;
+    WHILE @t <= @NumTests
+    BEGIN
+        DECLARE @TestName NVARCHAR(100) = 'Generated Test ' + CAST(@t AS NVARCHAR);
+        
+        INSERT INTO Tests (TestName, IsATest)
+        VALUES (@TestName, 1);
 
-    -- Clone ALL Zones from snapshot 0 to new snapshot
+        DECLARE @NewTestId INT = SCOPE_IDENTITY();
+        INSERT INTO @Tests (TestId) VALUES (@NewTestId);
+        INSERT INTO @TestStats (TestId) VALUES (@NewTestId);
+
+        SET @t += 1;
+    END
+
+    -- Step 4: Clone catalog data from snapshot 0 to new snapshot
+
+    -- Clone all Zones
     DECLARE @ZoneMap TABLE (OldZoneId INT, NewZoneId INT);
-    
     DECLARE @OldZoneId INT, @ZoneName NVARCHAR(100), @IsRelevant BIT;
     DECLARE ZoneCursor CURSOR FOR 
     SELECT DISTINCT ZoneId, ZoneName, IsRelevant 
@@ -77,16 +81,16 @@ BEGIN
     CLOSE ZoneCursor;
     DEALLOCATE ZoneCursor;
 
-    -- Clone ALL Questions from snapshot 0 to new snapshot
+    -- Clone all Questions and assign each to the test with the least questions so far
     DECLARE @QuestionMap TABLE (OldQuestionId INT, NewQuestionId INT);
-    DECLARE @OldQuestionId INT, @TestId INT, @QuestionText NVARCHAR(MAX), @QuestionIsRelevant BIT;
+    DECLARE @OldQuestionId INT, @QuestionText NVARCHAR(MAX), @QuestionIsRelevant BIT;
     DECLARE QuestionCursor CURSOR FOR
-    SELECT DISTINCT QuestionId, TestId, QuestionText, IsRelevant 
+    SELECT DISTINCT QuestionId, QuestionText, IsRelevant 
     FROM Questions 
     WHERE SnapshotId = 0;
 
     OPEN QuestionCursor;
-    FETCH NEXT FROM QuestionCursor INTO @OldQuestionId, @TestId, @QuestionText, @QuestionIsRelevant;
+    FETCH NEXT FROM QuestionCursor INTO @OldQuestionId, @QuestionText, @QuestionIsRelevant;
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
@@ -94,19 +98,28 @@ BEGIN
         IF @QuestionIsRelevant = 1
             SET @Score = CAST(RAND(CHECKSUM(NEWID())) * 100 AS INT);
 
+        DECLARE @TargetTestId INT;
+        SELECT TOP 1 @TargetTestId = TestId
+        FROM @TestStats
+        ORDER BY QuestionCount ASC;
+
         INSERT INTO Questions (SnapshotId, TestId, QuestionText, Score, IsRelevant)
-        VALUES (@NewSnapshotId, @TestId, @QuestionText, @Score, @QuestionIsRelevant);
+        VALUES (@NewSnapshotId, @TargetTestId, @QuestionText, @Score, @QuestionIsRelevant);
+
+        UPDATE @TestStats
+        SET QuestionCount = QuestionCount + 1
+        WHERE TestId = @TargetTestId;
 
         DECLARE @NewQuestionId INT = SCOPE_IDENTITY();
         INSERT INTO @QuestionMap (OldQuestionId, NewQuestionId) VALUES (@OldQuestionId, @NewQuestionId);
 
-        FETCH NEXT FROM QuestionCursor INTO @OldQuestionId, @TestId, @QuestionText, @QuestionIsRelevant;
+        FETCH NEXT FROM QuestionCursor INTO @OldQuestionId, @QuestionText, @QuestionIsRelevant;
     END
 
     CLOSE QuestionCursor;
     DEALLOCATE QuestionCursor;
 
-    -- Clone ALL ZonesQuestions relationships
+    -- Clone ZonesQuestions relationships
     INSERT INTO ZonesQuestions (SnapshotId, ZoneId, QuestionId)
     SELECT DISTINCT @NewSnapshotId, zm.NewZoneId, qm.NewQuestionId
     FROM ZonesQuestions zq
@@ -114,7 +127,7 @@ BEGIN
     JOIN @QuestionMap qm ON zq.QuestionId = qm.OldQuestionId
     WHERE zq.SnapshotId = 0;
 
-    -- Step 4: Clone subjects from snapshot 0 to new snapshot
+    -- Step 5: Clone Subjects to new snapshot
     DECLARE @SubjectMap TABLE (OldSubjectId INT, NewSubjectId INT);
     DECLARE SubjectCursor CURSOR FOR 
     SELECT SubjectId FROM @OriginalSubjectIds;
@@ -124,13 +137,11 @@ BEGIN
 
     WHILE @@FETCH_STATUS = 0
     BEGIN
-        -- Clone subject
         DECLARE @SubjectName NVARCHAR(100);
         SELECT @SubjectName = SubjectName 
         FROM Subjects 
         WHERE SubjectId = @OriginalSubjectId AND SnapshotId = 0;
 
-        -- Add validation for SubjectName
         IF @SubjectName IS NULL
         BEGIN
             SET @SubjectName = 'Generated Subject ' + CAST(@OriginalSubjectId AS NVARCHAR(10));
@@ -148,12 +159,11 @@ BEGIN
     CLOSE SubjectCursor;
     DEALLOCATE SubjectCursor;
 
-    -- Step 5: Clone SubjectZones relationships
+    -- Step 6: Clone SubjectZones relationships
     INSERT INTO SubjectZones (SnapshotId, SubjectId, ZoneId)
     SELECT @NewSnapshotId, sm.NewSubjectId, zm.NewZoneId
     FROM SubjectZones sz
     JOIN @SubjectMap sm ON sz.SubjectId = sm.OldSubjectId
     JOIN @ZoneMap zm ON sz.ZoneId = zm.OldZoneId
     WHERE sz.SnapshotId = 0;
-
 END
